b88a336cce50d53722edb1c13043d69d50a4b7bb
Felipe-Portas <felipeportas@hotmail.com>
2024-08-27 20:28:50 -0300

    finalizacao-logica-js-e-alteracao-nomenclaturas

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
 const textArea = document.querySelector(".text-area");
 const mensagem = document.querySelector(".mensagem");
 
-function btnEncriptar() {
-    const textoEncriptado = encriptar(textArea.value);
+function btnCriptografar() {
+    const textoEncriptado = criptografar(textArea.value);
     mensagem.value = textoEncriptado
     textArea.value = "";
 }
 
-function encriptar(stringEncriptada) {
-    let matrizCodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+function criptografar(stringEncriptada) {
+    let matrizCripto = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
     stringEncriptada = stringEncriptada.toLowerCase();
 
-    for(let i = 0; i < matrizCodigo.length; i++) {
-        if(stringEncriptada.includes(matrizCodigo[i][0])) {
-            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+    for(let i = 0; i < matrizCripto.length; i++) {
+        if(stringEncriptada.includes(matrizCripto[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCripto[i][0], matrizCripto[i][1]);
         }
     }
 
     return stringEncriptada;
 }
+
+function btnDescriptografar() {
+    const textoDesencriptado = descriptografar(textArea.value);
+    mensagem.value = textoDesencriptado
+    textArea.value = "";
+}
+
+function descriptografar(stringDesencriptada) {
+    let matrizCripto = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for(let i = 0; i < matrizCripto.length; i++) {
+        if(stringDesencriptada.includes(matrizCripto[i][1])) {
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCripto[i][1], matrizCripto[i][0]);
+        }
+    }
+
+    return stringDesencriptada;
+}    
